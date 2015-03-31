8f8be86c737e374ecc121663fc84e1b78ddb143e
banielTimes <daniel.sy89@gmail.com>
2015-04-01 00:55:05 +0200

    some refactoring and added angular material and bootstrap via bower

--- js/app.js
+++ js/app.js
@@ -21,31 +21,7 @@ var app = angular.module(appname, [
 ]);
 
 // -------------------
-// Services
-// -------------------
-/*app.service('DataService', function($http, $q) {
-    var _this = this;
-
-    this.loadData = function() {
-        var defer = $q.defer();
-
-        $http.get('./data/data.js')
-            .success(function(data) {
-                angular.extend(_this, data)
-                defer.resolve();
-            })
-            .error(function() {
-                defer.reject('Could not load data');
-            })
-
-        return defer.promise;
-    }
-
-    return _this;
-})*/
-
-// -------------------
-// Controllers
+// Controller
 // -------------------
 app.controller('CluesController', [
     '$scope',
@@ -56,11 +32,7 @@ app.controller('CluesController', [
         var cluesCtrl = this;
         $scope.bluescluesData = bluescluesData;
 
-        cluesCtrl.inputCode = {};
-        cluesCtrl.validCode = {};
-
         cluesCtrl.validateCode = function(inputCode) {
-
             cluesCtrl.validCode = false;
 
             angular.forEach(bluescluesData, function (clue) {
@@ -74,90 +46,7 @@ app.controller('CluesController', [
             });
         };
 
-        /*$scope.question = $filter('filter')(bluescluesData, function(question) {
-            return question.code == $scope.inputCode;
-        })[0];
-
-        console.log($scope.question);*/
     }
 ]);
-
-// -------------------
-// Directives
-// -------------------
-/*app.directive('question', function() {
-    return {
-        restrict: 'E',
-        scope: true,
-        templateUrl: 'question.html'
-    };
-});*/
-
-// -------------------
-// Services
-// -------------------
-/*app.factory('questionFactory', function($filter) {
-    var question;
-
-    return {
-        findQuestion: function(code, data) {
-            question = $filter('filter')(bluescluesData, function(question) {
-                return question.code == code;
-            })[0];
-
-            return question;
-        }
-    };
-})*/
-
-/*app.directive('validateCode', function() {
-    return {
-        require: ngModel,
-        link: function(scope, elem, attrs, ctrl) {
-            ctrl.$parsers.unshift(function(value))
-        }
-    }
-})*/
-
-
-/*app.directive('validateCode', function() {
-    return {
-        require: 'ngModel',
-        link: function(scope, elem, attrs, ctrl) {
-            ctrl.$parsers.unshift(function(value) {
-                if (value) {
-                    var valid = value === '123456';
-                    ctrl.$setValidity('invalidCode', valid);
-                }
-
-                return valid ? valid : undefined;
-            })
-        }
-    }
-});*/
-
-/*app.directive('validationTimeout', function($q, $timeout) {
-    return {
-         require: 'ngModel',
-         link: function(scope, elm, attrs, ctrl) {
-             var codes = ['123456'];
-
-             ctrl.$asyncValidators.code = function(modelValue, viewValue) {
-                 var def = $q.defer();
-
-                 $timeout(function() {
-                     //delayed response :P
-                     if (codes.indexOf(modelValue) === -1) {
-                        def.reject();
-                     } else {
-                        def.resolve();
-                     }
-                 }, 2000);
-
-                 return def.promise;
-             }
-         }
-    }
-});*/
 
 console.log('App booted up!');
